c11b97ffa58574b37aea3428e0281be1be321828
GretaFormica <greta.formica@gmail.com>
2023-02-01 16:25:44 +0100

    num + generazione con btn

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 //on load
-const griglia = document.getElementById("grid");
-generagriglia(griglia);
+const bottone = document.getElementById("btn");
+
+bottone.addEventListener(
+    'click',
+
+    function () {
+        const griglia = document.getElementById("grid");
+        generagriglia(griglia);
+    }
+)
+
 
 //funzioni
 
 function generagriglia(grid) {
+    grid.InnerHTML = "";
+
     for (let i = 0; i < 100; i++) {
 
         const box = document.createElement("div");
         box.classList.add("square");
+        box.append(i + 1);
 
         box.addEventListener(
             'click',
 
             function () {
                 this.classList.toggle("active")
+                console.log(i + 1)
             }
         )
 
         grid.append(box);
     }
+
 }
